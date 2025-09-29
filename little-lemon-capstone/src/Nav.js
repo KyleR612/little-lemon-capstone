d9b0e2f493119feaae0c91aee2eb77dc2452a2f2
Kyle Radant <radantk@yahoo.com>
2025-09-29 11:17:25 -0400

    Committing files

--- little-lemon-capstone/src/Nav.js
+++ little-lemon-capstone/src/Nav.js
@@ -7,6 +7,15 @@ function Nav() {
         <nav>
             <a href='/'></a>
             <img src={logo} alt='little lemon logo'/>
+
+
+            <ul class="container">
+                <li><a href='/'>Home</a></li>
+                <li><a href="/">About</a></li>
+                <li><a href="/">Services</a></li>
+                <li><a href="/">Menu</a></li>
+                <li><a href="/">Reservations</a></li>
+            </ul>
         </nav>
     );
 } 
