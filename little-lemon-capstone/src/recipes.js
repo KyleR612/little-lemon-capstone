d65f0c9e15c279a124d81509dce8c48b2b478830
Kyle Radant <radantk@yahoo.com>
2025-10-13 19:19:10 -0400

    Committing files

--- little-lemon-capstone/src/recipes.js
+++ little-lemon-capstone/src/recipes.js
@@ -1,25 +1,11 @@
-const recipes = [
-    {
-        id: 1,
-        title: "Greek Salad",
-        price: 12.99,
-        image: "little-lemon-capstone/little-lemon-capstone/src/Images/greek-salad.jfif",
-        description: "The famous greek salad filled with cripsy lettuce, peppers, olives and a Chicago style fetta cheese, garnished with crunchy garlic and rosemary croutons."
-    },
-    {
-        id: 2,
-        title: "Bruchetta",
-        price: 5.99,
-        image: "little-lemon-capstone/little-lemon-capstone/src/Images/bruchetta.jpg",
-        description: "Our Bruchetta is make from grilled bread that has been smeared with garlic and seasoned with salt and olive oil."
-    },
-    {
-        id: 3,
-        title: "Lemon Dessert",
-        price: 4.78,
-        image: "little-lemon-capstone/little-lemon-capstone/src/Images/lemon dessert.jfif",
-        description: "A beautiful lemon filled dessert, made from scratch and served to your liking."
-    }
-];
+import salad from '../src/Images/salad.jpg';
 
+const recipes = () => {
+    return(
+    <a href="/">
+    <img src={salad} alt='greek salad'/>
+    </a>
+   );
+}; 
+  
 export default recipes;
